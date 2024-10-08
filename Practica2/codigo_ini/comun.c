05c6bd7e5a0ad04f0f808e9e1d78bc293f2c01f9
antoniocenlaces <antoniocenlaces@gmail.com>
2024-10-08 10:37:02 +0000

    vuelta a hints.ai_socktype = SOCK_STREAM;

--- Practica2/codigo_ini/comun.c
+++ Practica2/codigo_ini/comun.c
@@ -126,7 +126,7 @@ struct addrinfo* obtener_struct_direccion(char *dir_servidor, char *servicio, ch
 
     // especificamos el tipo de socket deseado:
     // SOCK_STREAM (TCP), SOCK_DGRAM (UDP), etc.
-    hints.ai_socktype = 0;
+    hints.ai_socktype = SOCK_STREAM;
 
     if (f_verbose)
     {
